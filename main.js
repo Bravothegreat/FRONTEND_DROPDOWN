e774655a0472be024a22c132cc5241362f8f8f93
bravothegreat <okwudilijohngreat@gmail.com>
2024-03-02 01:09:41 +0100

    FRONTEND_DROPDOWN commit

---
+++ main.js
@@ -0,0 +1,14 @@
+
+const hamburger =document.querySelector(".menu_icon")
+
+const Navmenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active")
+  Navmenu.classList.toggle("active");
+})
+
+document.querySelectorAll("a").forEach(n => n.addEventListener("click",() => {
+  hamburger.classList.remove("active")
+  Navmenu.classList.remove("active")
+}))
